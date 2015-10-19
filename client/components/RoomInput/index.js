90576c8eea61e41e5adc9b5017293363e47552a8
John Grishin <r@exah.ru>
2015-10-19 13:23:32 +0300

    React-mention experiment [fail]

--- client/components/RoomInput/index.js
+++ client/components/RoomInput/index.js
@@ -1,7 +1,9 @@
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
+import { MentionsInput, Mention } from 'react-mentions';
 import { sendMessage } from '../../smartActions';
 import { roomInputChange, togglePreview } from '../../actions';
+import { EMOJI_REGEX, EMOJI_URL, EMOJI_IMAGES } from '../../../common/emoji';
 import './index.css';
 
 function onKeyPress(e, handler) {
@@ -21,28 +23,55 @@ function onClick(e, handler) {
   handler();
 }
 
+const fakeData = [
+  {
+    id: 'exah',
+    display: 'exah',
+  },
+  {
+    id: '+1',
+    display: '+1',
+  },
+  {
+    id: 'john',
+    display: 'john',
+  },
+];
+
 class RoomInput extends Component {
-  componentDidUpdate() {
-    const textarea = this.refs.textarea;
-    textarea.style.height = '';
-    textarea.style.height = `${textarea.scrollHeight}px`;
-  }
+  // componentDidUpdate() {
+  //   const textarea = this.refs.textarea;
+  //   textarea.style.height = '';
+  //   textarea.style.height = `${ textarea.scrollHeight }px`;
+  // }
 
   render() {
     const { dispatch, text, buttonEnabled, previewCollapsed } = this.props;
     return (
       <form className="room-actions"
         onSubmit={e => onClick(e, () => dispatch(sendMessage()))}>
-        <textarea
-          type="text"
-          ref="textarea"
+        <MentionsInput
+          className="room-actions-input"
           placeholder="Message..."
-          className="room-actions-input input"
+          style={{'flex': '1'}}
           onChange={e => dispatch(roomInputChange(e.target.value))}
           onKeyPress={e => onKeyPress(e, () => dispatch(sendMessage()))}
           rows="1"
-          value={text}
-        ></textarea>
+          markup={'**@__id__**'}
+          displayTransform={ (id, display, type) => (`@${ id }`) }
+          value={text}>
+          <Mention
+            trigger="@"
+            type="user"
+            data={fakeData}
+            renderSuggestion={ (id) => (`@${ id }`) } />
+
+          <Mention
+            trigger=":"
+            type="emoji"
+            data={fakeData}
+            renderSuggestion={ (id) => (`![](${ EMOJI_URL }/${ id })`) } />
+        </MentionsInput>
         <button
           className={`btn ${ previewCollapsed ? 'is-off' : ''}`}
           onClick={e => onClick(e, () => dispatch(togglePreview()))}
